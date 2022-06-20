2bd851fa13a64d49e6f1877c1b236f8df3bb3e3d
dalla@MSI <dallas.archer.bailey@gmail.com>
2022-06-20 19:04:54 -0400

    added password capability

--- models/index.js
+++ models/index.js
@@ -1,3 +1,10 @@
 const User = require('../models/User');
-
+const Post = require("./Post");
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
 module.exports = { User };
+module.exports = { User, Post };
