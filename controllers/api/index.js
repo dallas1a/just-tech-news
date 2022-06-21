0f16640678efa593b896476b41285df4348948ae
dalla@MSI <dallas.archer.bailey@gmail.com>
2022-06-21 17:52:00 -0400

    added homepage

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+const homeRoutes = require('../home-routes');
 
+router.use('/', homeRoutes);
 router.use('/comments', commentRoutes);
 
 router.use('/users', userRoutes);
